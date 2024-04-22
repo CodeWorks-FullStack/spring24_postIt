b0313dba0516804066f6bf449ef4446f90463337
Mick <beasticorn@gmail.com>
2024-04-22 14:15:45 -0600

    albums backend done

--- server/src/controllers/AlbumsController.js
+++ server/src/controllers/AlbumsController.js
@@ -8,8 +8,11 @@ export class AlbumsController extends BaseController {
   constructor() {
     super('api/albums')
     this.router
+      .get('', this.getAlbums)
+      .get('/:albumId', this.getOneById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createAlbum)
+      .delete('/:albumId', this.archiveAlbum)
   }
   async createAlbum(request, response, next) {
     try {
@@ -18,6 +21,36 @@ export class AlbumsController extends BaseController {
       albumData.creatorId = user.id
       const album = await albumsService.createAlbum(albumData)
       response.send(album)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getAlbums(request, response, next) {
+    try {
+      const albums = await albumsService.getAlbums()
+      response.send(albums)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getOneById(request, response, next) {
+    try {
+      const albumId = request.params.albumId
+      const album = await albumsService.getOneById(albumId)
+      response.send(album)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async archiveAlbum(request, response, next) {
+    try {
+      const albumId = request.params.albumId
+      const userId = request.userInfo.id
+      const message = await albumsService.archiveAlbum(albumId, userId)
+      response.send(message)
     } catch (error) {
       next(error)
     }
--- server/src/services/AlbumsService.js
+++ server/src/services/AlbumsService.js
@@ -1,12 +1,36 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 
 
 class AlbumsService {
+
+
   async createAlbum(albumData) {
     const album = await dbContext.Albums.create(albumData)
     await album.populate('creator')
     return album
+  }
+  async getAlbums() {
+    const albums = await dbContext.Albums.find().populate('creator')
+    return albums
+  }
+
+  async getOneById(albumId) {
+    const album = await dbContext.Albums.findById(albumId).populate('creator')
+    if (!album) throw new Error(`No album with the id: ${albumId}`)
+    return album
+  }
+
+  async archiveAlbum(albumId, userId) {
+    const albumToArchive = await this.getOneById(albumId)
+    if (albumToArchive.creatorId != userId) throw new Forbidden(`You cannot archive what is not yours`)
+
+    // await albumToArchive.deleteOne() // if you wanted to delete it
+    // albumToArchive.archived = true // make the change
+    albumToArchive.archived = !albumToArchive.archived // flips the bool
+    await albumToArchive.save() // save the changes
+    return `${albumToArchive.title} has been archived`
   }
 
 }
